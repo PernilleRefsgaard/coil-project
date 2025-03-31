beb688249e3c62de71742eea02ffed7804b94aa7
PernilleRefsgaard <Refsgaardpernille@gmail.com>
2025-03-31 11:28:30 +0200

    ..

--- js/script.js
+++ js/script.js
@@ -2,14 +2,6 @@
 
 
 let text = document.getElementById("text");
-let leaf = document.getElementById("leaf");
-let hill1 = document.getElementById("hill1");
-let hill2 = document.getElementById("hill2");
-let hill3 = document.getElementById("hill3");
-let hill4 = document.getElementById("hill4");
-let hill5 = document.getElementById("hill5");
-let tree = document.getElementById("tree");
-let tree4 = document.getElementById("tree4");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
